b7dc0da11743c14c7a7554c031b72b10c17f235d
glebmanaev <87305392+glebmanaev@users.noreply.github.com>
2023-04-29 18:35:15 +0300

    Create pso.py

---
+++ pso.py
@@ -0,0 +1,52 @@
+import random
+import numpy as np
+
+class PSO:
+    def __init__(self, objective_function, param_ranges, num_particles=10, max_iter=50, c1=2, c2=2, w=0.7):
+        self.objective_function = objective_function
+        self.param_ranges = param_ranges
+        self.num_particles = num_particles
+        self.max_iter = max_iter
+        self.c1 = c1
+        self.c2 = c2
+        self.w = w
+
+    def run(self):
+        # Initialize the particles and their velocities
+        particles = np.array([self._generate_particle() for i in range(self.num_particles)])
+        velocities = np.zeros((self.num_particles, len(self.param_ranges)))
+        best_particle_positions = particles.copy()
+        best_particle_scores = np.full(self.num_particles, -np.inf)
+        global_best_particle_position = None
+        global_best_particle_score = -np.inf
+
+        # Iterate for the maximum number of iterations
+        for i in range(self.max_iter):
+            # Evaluate the objective function for each particle
+            particle_scores = np.array([self.objective_function(particle) for particle in particles])
+
+            # Update the best positions and scores for each particle
+            for j in range(self.num_particles):
+                if particle_scores[j] > best_particle_scores[j]:
+                    best_particle_positions[j] = particles[j]
+                    best_particle_scores[j] = particle_scores[j]
+                if particle_scores[j] > global_best_particle_score:
+                    global_best_particle_position = particles[j]
+                    global_best_particle_score = particle_scores[j]
+
+            # Update the velocities and positions of the particles
+            for j in range(self.num_particles):
+                r1 = random.uniform(0, 1)
+                r2 = random.uniform(0, 1)
+                velocities[j] = self.w * velocities[j] \
+                                 + self.c1 * r1 * (best_particle_positions[j] - particles[j]) \
+                                 + self.c2 * r2 * (global_best_particle_position - particles[j])
+                particles[j] += velocities[j]
+
+                # Ensure that the particles stay within the specified parameter ranges
+                particles[j] = np.clip(particles[j], self.param_ranges[:, 0], self.param_ranges[:, 1])
+
+        return global_best_particle_position, global_best_particle_score
+
+    def _generate_particle(self):
+        return np.array([random.uniform(param_range[0], param_range[1]) for param_range in self.param_ranges])

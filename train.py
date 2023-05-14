99f842aba33aa186d0910d2ca9e97f574856da2e
glebmanaev <87305392+glebmanaev@users.noreply.github.com>
2023-05-14 22:10:39 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,136 +1,143 @@
 import torch
 import numpy as np
 import pandas as pd
-from transformers import DistilBertTokenizer, DistilBertForSequenceClassification, DistilBertConfig
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification, TrainingArguments, Trainer, DataCollatorWithPadding, EarlyStoppingCallback
 from sklearn.model_selection import train_test_split
 from pso import PSO
+import evaluate
 
 
-def eval_model(model, val_dataloader):
-    model.eval()
-        with torch.no_grad():
-            for batch in val_dataloader:
-                batch = {k: v.to(device) for k, v in zip(['input_ids', 'attention_mask', 'labels'],batch)}
+class CustomTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.get("labels")
+        # forward pass
+        outputs = model(**inputs)
+        logits = outputs.get("logits")
+        # compute custom loss
+        loss_fct = torch.nn.CrossEntropyLoss(weight=class_weights).to(device)
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+        return (loss, outputs) if return_outputs else loss
+    
+accuracy = evaluate.load("accuracy")
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return accuracy.compute(predictions=predictions, references=labels)
 
-                outputs = model(**batch)
-                batch_loss = outputs.loss
-                batch_logits = outputs.logits
-                val_loss += batch_loss.item()
-                num_val_batches += 1
-
-                _, predictions = torch.max(batch_logits, dim=1)
-                num_correct += (predictions == batch["labels"]).sum().item()
-                num_total += batch["labels"].shape[0]
-
-        val_accuracy = num_correct / num_total
-        val_loss /= num_val_batches
-    return val_accuracy, val_loss
+def preprocessing(x):
+    tokenized_x = tokenizer(x['text'], truncation=True, padding=True)
+    tokenized_x['labels'] = x['label']
+    return tokenized_x
 
 
-def train_and_evaluate_distilbert_model(params, train_dataloader, val_dataloader, num_labels, test_dataloader=None, save_model=False):
+
+def train_and_evaluate_distilbert_model(params, tokenizer, tokenized_dataset, num_labels, id2label, label2id, save_model=False):
     '''Train and evaluate a DistilBERT model with the given hyperparameters.'''
-    learning_rate, dropout_prob, num_epochs = params
+    learning_rate, dropout_prob, num_epochs = params[0], 0, 1
 
-    # Load the DistilBERT model
-    config = DistilBertConfig(dropout=dropout_prob, attention_dropout=dropout_prob, seq_classif_dropout=dropout_prob*1.5)
-    model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=num_labels, dropout=dropout_prob, attention_dropout=dropout_prob, seq_classif_dropout=dropout_prob*1.5)
-    model.to(device)
-    # Train the model
-    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
-    model.train()
-    for epoch in range(int(num_epochs)):
-        for batch in train_dataloader:
-            batch = {k: v.to(device) for k, v in zip(['input_ids', 'attention_mask', 'labels'],batch)}
-            outputs = model(**batch)
-            loss = outputs.loss
-            loss.backward()
-            optimizer.step()
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+    model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=num_labels, id2label=id2label, label2id=label2id , dropout=dropout_prob, attention_dropout=dropout_prob, seq_classif_dropout=dropout_prob*1.5)
+    for name, param in list(model.named_parameters())[:-4]:
+        param.requires_grad = False
+    #for name, param in model.named_parameters():
+    #    print(name, param.requires_grad)
+    #quit()
 
-        # Evaluate the model on the validation set each epoch
-        val_loss = 0
-        num_val_batches = 0
-        num_correct = 0
-        num_total = 0
-        val_accuracy, val_loss = eval_model(model, val_dataloader)
-        print(f"Epoch {epoch+1} | Train Loss: {loss.item():.4f} | Val Loss: {val_loss:.4f} | Val Accuracy: {val_accuracy:.4f}")
+    training_args = TrainingArguments(
+    output_dir="NLP_project",
+    learning_rate=learning_rate,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
+    num_train_epochs=num_epochs,
+    weight_decay=0.01,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    metric_for_best_model="eval_accuracy",
+    greater_is_better=True,
+    )
 
+    trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset['train'],
+    eval_dataset=tokenized_dataset['val'],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+    callbacks=[EarlyStoppingCallback(early_stopping_patience=3)])
+
+    trainer.train()
+    
     if save_model:
-        if test_dataloader is not None:
-            print("\nEvaluating the final model on a test set")
-            test_accuracy, _ = eval_model(model, test_dataloader)
-            print(f"Accuracy on the test set: {test_accuracy:.4f}")
+        print("\nEvaluating the final model on a test set")
+        test_accuracy = trainer.evaluate(tokenized_dataset['test'])['eval_accuracy']
+        print(f"Accuracy on the test set: {test_accuracy:.4f}")
         print("\nSaving the final model")
         model.save_pretrained("distilbert_model_best_params")
-    return val_accuracy
 
+    return trainer.evaluate()['eval_accuracy']
+    
 
 def objective_function(params):
     '''Objective function for the PSO algorithm.'''
-    return -train_and_evaluate_distilbert_model(params, train_dataloader, val_dataloader, num_labels)
+    return -train_and_evaluate_distilbert_model(params, tokenizer, tokenized_dataset, num_labels, id2label, label2id)
 
 
 
 device = torch.device("cuda")
 
-# Define the text classification dataset
-df = pd.read_csv("mtsamples.csv")
+df = pd.read_csv("truncated_mtsamples.csv")
+
 df.dropna(inplace=True) # Drop any rows with missing values
 texts = df['transcription'].tolist() # a list of text samples
-labels = df['medical_specialty'].astype('category').cat.codes.tolist() # a list of corresponding labels
-num_labels = df['medical_specialty'].nunique()
+labels = df['medical_specialty'].astype('category').cat.codes.tolist()
 
-"""
-# Retrieve the medical specialty from an integer code
-code = 1  # Example code
-medical_specialty = categories.cat.categories[code]
-print("Medical specialty:", medical_specialty)
-"""
+train_texts, val_texts, train_labels, val_labels = train_test_split(texts, labels, test_size=.3)
+val_texts, test_texts, val_labels, test_labels = train_test_split(val_texts, val_labels, test_size=.3)
+
+# make datasets of format dict(label=..., text=...)
+dataset = {}
+dataset['train'] = [{"label": label, "text": text} for label, text in zip(train_labels, train_texts)]
+dataset['val'] = [{"label": label, "text": text} for label, text in zip(val_labels, val_texts)]
+dataset['test'] = [{"label": label, "text": text} for label, text in zip(test_labels, test_texts)]
+
+# tokenize datasets
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+tokenized_dataset = {}
+tokenized_dataset['train'] = [preprocessing(x) for x in dataset['train']]
+tokenized_dataset['val'] = [preprocessing(x) for x in dataset['val']]
+tokenized_dataset['test'] = [preprocessing(x) for x in dataset['test']]
+
+label2id = {v: i for i, v in enumerate(df['medical_specialty'].astype('category').cat.categories)}
+id2label = {i: v for i, v in enumerate(df['medical_specialty'].astype('category').cat.categories)}
+
+class_counts = df['medical_specialty'].value_counts()
+num_labels = len(class_counts)
+# class_weights = 1 / torch.tensor(class_counts, dtype=torch.float) # used for weighted loss
 
 # Split the dataset into training, validation and test sets
 train_texts, val_texts, train_labels, val_labels = train_test_split(texts, labels, test_size=.3)
 val_texts, test_texts, val_labels, test_labels = train_test_split(val_texts, val_labels, test_size=.3)
 
-# Tokenize the training and validation sets
-tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-train_encodings = tokenizer(train_texts, truncation=True, padding=True)
-val_encodings = tokenizer(val_texts, truncation=True, padding=True)
-test_encodings = tokenizer(test_texts, truncation=True, padding=True)
-
-train_dataset = torch.utils.data.TensorDataset(
-    torch.tensor(train_encodings['input_ids']),
-    torch.tensor(train_encodings['attention_mask']),
-    torch.tensor(train_labels)
-)
-val_dataset = torch.utils.data.TensorDataset(
-    torch.tensor(val_encodings['input_ids']),
-    torch.tensor(val_encodings['attention_mask']),
-    torch.tensor(val_labels)
-)
-test_dataset = torch.utils.data.TensorDataset(
-    torch.tensor(test_encodings['input_ids']),
-    torch.tensor(test_encodings['attention_mask']),
-    torch.tensor(test_labels)
-)
-train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
-val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=False)
-test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
-
 
 # Define the hyperparameter ranges to search over
 param_ranges = np.array([
-    [1e-6, 1e-3],  # learning rate
-    [0, 0.3],    # dropout probability
-    [5, 40]         # number of epochs
+    [1e-8, 1e-3],  # learning rate
+#    [0, 0.1],    # dropout probability
+#    [1, 3]         # number of epochs
 ])
 
 # Set up the PSO algorithm with the objective function and hyperparameter ranges
-pso = PSO(objective_function, param_ranges, num_particles=20, max_iter=100)
+pso = PSO(objective_function, param_ranges, num_particles=10, max_iter=5)
 
 # Run the PSO algorithm to find the optimal hyperparameters
-best_params, best_score, logs = pso.run()
+best_params, best_score = pso.run()
 pso.plot_logs()
 
+#best_params = [1e-4, 0, 5]
+
 # Train the DistilBERT model with the optimal hyperparameters on the entire dataset
-print(f"Best params: \nLearning rate {best_params[0]} \nDropout probability {best_params[1]} \nNum epochs {best_params[2]}")
+# print(f"Best params: \nLearning rate {best_params[0]} \nDropout probability {best_params[1]} \nNum epochs {best_params[2]}")
 print(f"\nTraining the final model with best params")
-train_and_evaluate_distilbert_model(best_params, train_dataloader, val_dataloader, num_labels, save_model=True)
+train_and_evaluate_distilbert_model(best_params, tokenizer, tokenized_dataset, num_labels, id2label, label2id, save_model=True)
